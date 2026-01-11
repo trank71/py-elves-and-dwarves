dce311a2619df32fc748e72b1550ecf386aa1e5d
Ambra <trank13f@gmail.com>
2026-01-11 13:57:14 +0100

    reorganithation structure and created imports, realise deast_of_the_dwarves func

--- app/main.py
+++ app/main.py
@@ -1,114 +1,10 @@
-from abc import ABC, abstractmethod
-
-
-class Player(ABC):
-    def __init__(self, nickname: str) -> None:
-        self.nickname = nickname
-
-    @abstractmethod
-    def get_rating(self) -> int:
-        pass
-
-    @abstractmethod
-    def player_info(self) -> str:
-        pass
-
-
-class Elf(Player):
-    def __init__(self
-                 , nickname: str
-                 , musical_instrument: str
-                 ) -> None:
-        super().__init__(nickname)
-        self._musical_instrument = musical_instrument
-
-    def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing"
-              f" a song on the {self._musical_instrument}")
-
-
-class Dwarf(Player):
-    def __init__(self
-                 , nickname: str
-                 , favourite_dish: str
-    ) -> None:
-        super().__init__(nickname)
-        self._favourite_dish = favourite_dish
-
-    def eat_favourite_dish(self) -> None:
-        print(f"{self.nickname} "
-              f"is eating {self._favourite_dish}")
-
-
-class ElfRanger(Elf):
-    def __init__(self
-                 , nickname: str
-                 , musical_instrument: str
-                 , bow_level: int
-                 ) -> None:
-        super().__init__(nickname, musical_instrument)
-        self._bow_level = bow_level
-
-    def player_info(self) -> str:
-        return (f"Elf ranger {self.nickname}"
-                f". {self.nickname} has bow of "
-                f"the {self._bow_level} level")
-
-    def get_rating(self) -> int:
-        return 3 * self._bow_level
-
-
-class Druid(Elf):
-    def __init__(self
-                 , nickname: str
-                 , musical_instrument: str
-                 , favourite_spell: str
-                 ) -> None:
-        super().__init__(nickname, musical_instrument)
-        self._favourite_spell = favourite_spell
-
-    def player_info(self) -> str:
-        return (f"Druid {self.nickname}"
-                f". {self.nickname} has a favourite "
-                f"spell: {self._favourite_spell}")
-
-    def get_rating(self) -> int:
-        return len(self._favourite_spell)
-
-
-class DwarfWarrior(Dwarf):
-    def __init__(self
-                 , nickname: str
-                 , favourite_dish: str
-                 , hummer_level: int
-                 ) -> None:
-        super().__init__(nickname, favourite_dish)
-        self._hummer_level = hummer_level
-
-    def player_info(self) -> str:
-        return (f"Dwarf warrior {self.nickname}"
-                f". {self.nickname} has a hummer "
-                f"of the {self._hummer_level} level")
-
-    def get_rating(self) -> int:
-        return self._hummer_level + 4
-
-
-class DwarfBlacksmith(Dwarf):
-    def __init__(self
-                 , nickname: str
-                 , favourite_dish: str
-                 , skill_level: int
-                 ) -> None:
-        super().__init__(nickname, favourite_dish)
-        self._skill_level = skill_level
-
-    def player_info(self) -> str:
-        return (f"Dwarf blacksmith {self.nickname} "
-                f"with skill of the {self._skill_level} level")
-
-    def get_rating(self) -> int:
-        return self._skill_level
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.elves.elf_ranger import ElfRanger
+from app.players.elves.druid import Druid
+from app.players.dwarves.dwarf import Dwarf
+from app.players.dwarves.dwarf_warrior import DwarfWarrior
+from app.players.dwarves.dwarf_blacksmith import DwarfBlacksmith
 
 
 def calculate_team_total_rating(team: list[Player]) -> int:
@@ -118,3 +14,8 @@ def calculate_team_total_rating(team: list[Player]) -> int:
 def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
+
+
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
+    for dwarf in dwarves:
+        dwarf.eat_favourite_dish()
