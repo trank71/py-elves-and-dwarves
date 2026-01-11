da14fc9cb537bea06fb033766ecdc263e14c56a3
Ambra <trank13f@gmail.com>
2026-01-11 13:04:44 +0100

    created Dwarf class and eat_favourite_dish method

--- app/main.py
+++ app/main.py
@@ -26,3 +26,13 @@ class Elf(Player):
         print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
 
 
+class Dwarf(Player):
+    def __init__(self
+                 , nickname: str
+                 , favourite_dish: str
+    ) -> None:
+        super().__init__(nickname)
+        self._favourite_dish = favourite_dish
+
+    def eat_favourite_dish(self) -> None:
+        print(f"{self.nickname} is eating {self._favourite_dish}")
