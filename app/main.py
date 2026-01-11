4a9b289058e9fa6b5960b1b2f986be4670d50436
Ambra <trank13f@gmail.com>
2026-01-11 13:47:10 +0100

    create an elves_concert func

--- app/main.py
+++ app/main.py
@@ -113,3 +113,8 @@ class DwarfBlacksmith(Dwarf):
 
 def calculate_team_total_rating(team: list[Player]) -> int:
     return sum(player.get_rating() for player in team)
+
+
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
+        elf.play_elf_song()
