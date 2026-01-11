bc5abaef458f6b55158aa8f2dfc2b859282f3aef
Ambra <trank13f@gmail.com>
2026-01-11 13:00:36 +0100

    created ELF class with play_elf_song method

--- app/main.py
+++ app/main.py
@@ -12,3 +12,17 @@ class Player(ABC):
     @abstractmethod
     def player_info(self) -> str:
         pass
+
+
+class Elf(Player):
+    def __init__(self
+                 , nickname: str
+                 , musical_instrument: str
+                 ) -> None:
+        super().__init__(nickname)
+        self._musical_instrument = musical_instrument
+
+    def play_elf_song(self) -> None:
+        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
+
+
